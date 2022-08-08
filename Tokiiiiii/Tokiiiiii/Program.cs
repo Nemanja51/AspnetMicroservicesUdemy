8920700e932fd1546004a86c1724f8bbfaf4e62d
Test <test.test@yahoo.com>
2022-08-08 13:25:22 +0200

    basket done a lot

---
+++ Tokiiiiii/Tokiiiiii/Program.cs
@@ -0,0 +1,22 @@
+﻿while (true)
+{
+    Console.WriteLine("What is your name?");
+    var name = Console.ReadLine();
+
+    Console.WriteLine($"Hi {name}");
+    Thread.Sleep(1000);
+
+    Console.WriteLine($"How old are you?");
+    var age = Convert.ToInt32(Console.ReadLine());
+
+    if (age > 18)
+    {
+        Console.WriteLine($"You are biiiiiiig {name}!!");
+    }
+    else
+    {
+        Console.WriteLine($"You are babyyyyyyy {name}!!");
+    }
+
+    Console.ReadLine();
+}
