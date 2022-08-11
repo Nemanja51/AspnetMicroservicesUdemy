653b9d51c51f0ee995383a990ea3390c7a6c6683
Test <test.test@yahoo.com>
2022-08-11 11:55:16 +0200

    initial ordering api commit

---
+++ src/Services/Basket/Basket.API/GrpsServices/DiscountGrpcService.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grpc.Protos;
+
+namespace Basket.API.GrpsServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProto;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProto)
+        {
+            _discountProto = discountProto;
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName) 
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+
+            return await _discountProto.GetDiscountAsync(discountRequest);
+        }
+    }
+}
